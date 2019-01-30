cff98ba8c913a1d021acec235936a077c47e11a5
IAmTurbanMan <rashidul.kader@gmail.com>
2019-01-29 20:27:59 -0800

    Initial upload for Week 3 homework/lab

---
+++ Week 3/H2P1.py
@@ -0,0 +1,20 @@
+#H2P1: functions
+
+#ask for and store age
+age = int (input ("Enter your age: "))
+
+#calculate and store age in 10 years
+futureAge = int (age + 10)
+
+#print greeting giving user a sense of impending doom
+print ("You will be {} years old in ten years! You're getting old".format (futureAge))
+
+#ask for and store Fahrenheit
+temperatureF = float (input ("Enter the current temperature in Fahrenheit: "))
+
+#calculate and store Celcius
+temperatureC = float ((temperatureF - 32.0) * (5.0 / 9.0))
+
+#print temperature in Celcius
+print ("It is {:.1f} degrees celcius.".format (temperatureC))
+
---
+++ Week 3/H2P2.py
@@ -0,0 +1,29 @@
+#H2P2: Book learnin'
+
+#input and store book information
+bookName = input ("What is your favorite book? ")
+bookAuthor = input ("Who wrote the book? ")
+bookPublishYear = int (input ("What year was the book published? "))
+bookPages = int (input ("How many pages does the book have? "))
+
+#get current year and store
+import datetime
+now = datetime.datetime.now ()
+currentYear = (now.year)
+
+#calculate and store age of book
+bookAge = (currentYear - bookPublishYear)
+
+#conditional statements for age
+if (bookAge < 10):
+    print ("This book is younger than ten years.")
+else:
+    print ("This book is at least ten years old.")
+
+#conditional statements for length
+if (bookPages < 100):
+    print ("This is a short book.")
+elif (bookPages >= 100 and bookPages < 300):
+    print ("This is an average book.")
+else:
+    print ("This is a long book.")
