ef64dd0895519d3d9bc55132f6303b3165016497
Rash Kader <rashidul.kader@gmail.com>
2019-03-18 19:59:04 -0700

    passwords saving, random password generator

---
+++ Password Manager/Main.py
@@ -0,0 +1,51 @@
+#Password manager
+
+import random
+import string
+
+#declarations
+
+passList = []
+#user input for data to be saved
+def newPassword():
+    website = input ("Please enter website: ")
+    user = input ("Please enter your username: ")
+    password = input ("Please enter your password: ")
+    if password == "random":
+        digits = int(input("Please enter number of digits: "))
+        randomPassword = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits ) for _ in range(digits))
+        #print ("random password: {}".format (randomPassword))
+        password = randomPassword
+    else:
+        pass
+
+    # print (website)
+    # print (user)
+    # print (password)
+
+    websiteEntry = dict([("website", website), ("username", user), ("password", password)])
+
+    #print (websiteEntry)
+
+    passList.append(websiteEntry)
+
+    print(passList)
+
+while True:
+    
+    print ('''
+    Welcome to the password manager.
+    new : create a new password
+    show: look up password
+    exit: exit the program
+    ''')
+
+    command = input('Please enter a command: ')
+    if command == "new":
+        newPassword()
+    elif command == "exit":
+        break
+    elif command == "show":
+        pass
+    else:
+        print ("Please enter a valid command")
