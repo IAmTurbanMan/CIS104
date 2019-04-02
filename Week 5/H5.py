31cfaa92569d442e9fb7f332a5f8e5d8de95ac72
Rash Kader <rashidul.kader@gmail.com>
2019-04-01 20:53:42 -0700

    added sort function and pickles!!!

--- Week 5/H5.py
+++ Week 5/H5.py
@@ -1,17 +1,11 @@
 import json
-
-aSong = {
-    "Title": None,
-    "Artist": None,
-    "Album": None,
-    "Track": None,
-    "Year": None,
-    "Genre": None
-}
+import pickle
 
 songList = []
 
 def addSong():
+    aSong = {}
+
     aSong["Title"] = input("Enter song title: ")
     aSong["Artist"] = input("Enter artist name: ")
     aSong["Album"] = input("Enter album title: ")
@@ -19,31 +13,36 @@ def addSong():
     aSong["Year"] = input("Enter release year: ")
     aSong["Genre"] = input("Enter genre: ")
 
-    #unsuccesful attempt at adding the dictionary to a list
+    #succesful attempt at adding the dictionary to a list!!!
     songList.append(aSong)
 
 def saveSong():
     #successful attempt at writing each element in the list to the file
-    if len(songList) > 0:
-        with open("MusicDB.txt", "w") as f:
-            for item in songList:
-                f.write("{}\n".format (item))
-    elif len(songList) == 0:
-        print("You must add a song first!")
+
+    pickleOut = open("MusicDB.pickle", "wb")
+    pickle.dump(songList, pickleOut)
+    pickleOut.close()
+
+    # if len(songList) > 0:
+    #     with open("MusicDB.txt", "w") as f:
+    #         for item in songList:
+    #             f.write("{}\n".format (item))
+    # elif len(songList) == 0:
+    #     print("You must add a song first!")
 
 def listSong():
-    count = 0
-    f = open("MusicDB.txt", "r")
-    for entry in f:
-        emptyEntry = entry.replace('"', '').strip("{").strip("}").strip(",").split("}{")
-    for s in emptyEntry:
-        count = count + 1
-        print("Song #{}: {}\n".format (count, s))
+    print (*songList, sep = "\n")
 
 def menu():
     print("add : Add a new song to the database")
     print("list: List dong in the database")
     print("save: Save the database")
+    print("sort: Sort the database by title/artist/release year")
     print("help: Display this menu")
     print("exit: Exit the program")
 
+def loadSong():
+    unpickle = []
+    with open("MusicDB.pickle", "rb") as f:
+        unpickle = pickle.load(f)
+    return unpickle
--- Week 5/MusicDB.py
+++ Week 5/MusicDB.py
@@ -1,11 +1,15 @@
 import json
 import H5
 
+songList = []
+
+songList = H5.loadSong()
+print (songList)
 H5.menu()
 
 while True:
     print ("\n")
-    cmd = input("Enter a command from the list above: ")
+    cmd = input("Enter a command: ")
 
     if cmd == "add":
         H5.addSong()
@@ -19,12 +23,24 @@ while True:
         print("The database is full!")
 
     elif cmd == "list":
-        H5.listSong()
-        print ("\n")
+        print (*songList, sep = "\n")
+        # H5.listSong()
+        # print ("\n")
 
     elif cmd == "help":
         H5.menu()
         print ("\n")
+
+    elif cmd == "sort":
+        sortBy = input ("What do you want to sort by? title/artist/release: ")
+        from operator import itemgetter
+        if (sortBy == "title"):
+            songList = sorted(songList, key = itemgetter('Title'))
+        if (sortBy == "artist"):
+            songList = sorted(songList, key = itemgetter('Artist'))
+        if (sortBy == "release"):
+            songList = sorted(songList, key = itemgetter('Year'))
+
 
     elif cmd == "exit":
         break
